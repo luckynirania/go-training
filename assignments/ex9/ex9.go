5c95c7f58acd94c14f87b3bcc8568966cf340cb1
Lokesh Nirania <lknmahan@gmail.com>
2021-05-24 18:31:21 +0530

    ex9 all methods done

--- assignments/ex9/ex9.go
+++ assignments/ex9/ex9.go
@@ -15,30 +15,50 @@ func check(err error) {
 func main() {
 	client := httpTraining.Client{Hostname: "https://api-ldej-nl.el.r.appspot.com"}
 
-	// GetThings
-	resp, err := client.GetThingOnUUID("97fb41b1-0bdc-488a-9e04-b34eb70d919d")
+	// GetALl
+	resp, err := client.GetAll()
 	if err != nil {
 		check(err)
 	}
-	fmt.Printf("%v", resp)
+	for key, value := range *resp {
+		fmt.Println(key, value)
+	}
+	fmt.Println()
+	// ---------------------------------------------------------------------------------------------
+
+	// Delete
+	err = client.DeleteThing("12db965a-5746-40f1-a3b0-5763ed9269d4")
+	if err != nil {
+		check(err)
+	}
+	fmt.Println("200 OK")
+	fmt.Println()
+	// ---------------------------------------------------------------------------------------------
+
+	// GetThing usung UUID
+	respi, err := client.GetThingOnUUID("97fb41b1-0bdc-488a-9e04-b34eb70d919d")
+	if err != nil {
+		check(err)
+	}
+	fmt.Printf("%v", respi)
 	fmt.Println()
 	// ---------------------------------------------------------------------------------------------
 
 	// Post a new thing
-	resp, err = client.CreateThing("Lokesh", "Posting here")
+	respi, err = client.CreateThing("Lokesh", "Posting here")
 	if err != nil {
 		check(err)
 	}
-	fmt.Printf("%v", resp)
+	fmt.Printf("%v", respi)
 	fmt.Println()
-	// ---------------------------------------------------------------------------------------------
+	// // ---------------------------------------------------------------------------------------------
 
-	// PUT a Thing
-	resp, err = client.PutThing("97fb41b1-0bdc-488a-9e04-b34eb70d919d", "Lokesh", "Putting here")
+	// // PUT a Thing
+	respi, err = client.PutThing("97fb41b1-0bdc-488a-9e04-b34eb70d919d", "Lokesh", "Putting here")
 	if err != nil {
 		check(err)
 	}
-	fmt.Printf("%v", resp)
+	fmt.Printf("%v", respi)
 	fmt.Println()
 	// ---------------------------------------------------------------------------------------------
 }
